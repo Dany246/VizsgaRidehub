00bfde8f35bee267e970f80dd6fefd3eaf2de34a
niezz01 <145447146+niezz01@users.noreply.github.com>
2025-03-11 08:39:57 +0100

    fix

--- resources/js/Pages/Drivers.jsx
+++ resources/js/Pages/Drivers.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import { Head, Link } from "@inertiajs/react";
 import Navbar from "@/Components/Navbar";
 import Drivercard from "@/Components/Drivercard";
-import DrivercardK from "@/Components/DrivercardK";
 
 const Driverspage = ( {auth}) => {
     return (
@@ -13,7 +12,6 @@ const Driverspage = ( {auth}) => {
         <h1 className="text-4xl font-bold p-12 text-center">Drivers</h1> 
         <div className="flex flex-wrap gap-4 justify-center">
         <Drivercard/>
-        <DrivercardK/>
         </div>
         
 
