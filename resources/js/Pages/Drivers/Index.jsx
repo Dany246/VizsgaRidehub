7102b1be73acc5958880eb8cdcf8b0885e6227a5
niezz01 <145447146+niezz01@users.noreply.github.com>
2025-03-13 12:38:58 +0100

    route fix

--- resources/js/Pages/Drivers/Index.jsx
+++ resources/js/Pages/Drivers/Index.jsx
@@ -18,7 +18,7 @@ const Driverspage = ({ auth, drivers }) => {
                 )}
             </div>
             <div className={`text-center pt-32`}>
-                <p className="text-2xl font-bold text-white"><a className="duration-500 hover:text-3xl" href="/order">⮚ Order a ride<span className="bg-orange-500 text-black p-1 rounded-lg">Here</span> ⮘</a></p>
+                <p className="text-2xl font-bold text-white"><a className="duration-500 hover:text-3xl" href="/orders">⮚ Order a ride<span className="bg-orange-500 text-black p-1 rounded-lg">Here</span> ⮘</a></p>
             </div>
         </div>
     );
