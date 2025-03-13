375ebdc90401f07626406932e44f35cc21c640f8
Dany246 <145447252+Dany246@users.noreply.github.com>
2025-03-13 11:22:02 +0100

    design dahsboard

--- resources/js/Components/NavLink.jsx
+++ resources/js/Components/NavLink.jsx
@@ -12,8 +12,8 @@ export default function NavLink({
             className={
                 'inline-flex items-center border-b-2 px-1 pt-1 text-sm font-medium leading-5 transition duration-150 ease-in-out focus:outline-none ' +
                 (active
-                    ? 'border-red-400 text-yellow-600 focus:border-red-800 focus'
-                    : 'border-transparent text-yellow-600 hover:border-red-400 hover:text-yellow-700 focus:border-red-600 focus:text-yellow-700') +
+                    ? 'border-red-400 text-orange-700 focus:border-red-800 focus'
+                    : 'border-transparent text-orange-700 hover:border-red-400 hover:text-yellow-700 focus:border-red-600 focus:text-yellow-700') +
                 className
             }
         >
